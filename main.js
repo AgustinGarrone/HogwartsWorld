4b2f5d4ed02a9e6cc2eab7c8c834396d58444271
AgustinGarrone <agustinngarrone@hotmail.com>
2022-01-28 04:59:27 -0300

    Nav , estilos y fuentes modificados.

--- main.js
+++ main.js
@@ -3,23 +3,36 @@ const api="https://hp-api.herokuapp.com/api/characters"
 console.log(api)
 var resultado=document.querySelector(".searchbar")
 var boton=$(".searchbarButton")
-var cardsection=document.createElement("section")
-cardsection.classList.add("resultscard")
+var cardsection1=document.createElement("section")
+var cardsection2=document.createElement("section")
+cardsection1.classList.add("resultscard")
+cardsection2.classList.add("resultscard2")
 var mibaraja=[]
 $.get(api,function(response,status) {
     if (status==="success") {
         function busqueda(buscar) {
 
-            document.querySelector(".harryapp").appendChild(cardsection)
-            cardsection.innerHTML=""
+            document.querySelector(".harryapp").appendChild(cardsection1)
+            cardsection1.innerHTML=""
+            cardsection2.innerHTML=""
+            let cuenta=0
             for (personaje of response) {
                 if (personaje.name.toUpperCase().includes(buscar)) {
                     if (personaje.image!="") {
-                        cardsection.innerHTML+=`<div class=character> <img class=characterimg src=${personaje.image}></img><p class=charactername>${personaje.name}</p> <button value="${personaje.name}" class=characterbutton>Add me</button></div>` 
-                        
+                        if (cuenta<=5) {
+                            cardsection1.innerHTML+=`<div class=character> <img class=characterimg src=${personaje.image}></img><p class=charactername>${personaje.name}</p> <button value="${personaje.name}" class=characterbutton>Add me</button></div>` 
+                            cuenta++
+                        } else {
+                            document.querySelector(".harryapp").appendChild(cardsection2)
+                            cardsection2.innerHTML+=`<div class=character> <img class=characterimg src=${personaje.image}></img><p class=charactername>${personaje.name}</p> <button value="${personaje.name}" class=characterbutton>Add me</button></div>` 
+                        }
                     }
-                    else {
-                        cardsection.innerHTML+=`<div class=character> <img class=characterimg src="./img/desconocida.png"></img><p class=charactername>${personaje.name}</p><button value="${personaje.name}"  class=characterbutton>Add me</button></div>` 
+                    else if (cuenta<=5) {
+                        cardsection1.innerHTML+=`<div class=character> <img class=characterimg src="./img/desconocida.png"></img><p class=charactername>${personaje.name}</p><button value="${personaje.name}"  class=characterbutton>Add me</button></div>` 
+                        cuenta++
+                    } else {
+                        document.querySelector(".harryapp").appendChild(cardsection2)
+                        cardsection2.innerHTML+=`<div class=character> <img class=characterimg src="./img/desconocida.png"></img><p class=charactername>${personaje.name}</p><button value="${personaje.name}"  class=characterbutton>Add me</button></div>` 
                     }
                 }
             }
